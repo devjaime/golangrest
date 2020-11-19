58428fb72d5302dbcc6772fafd32b56f1c4ac43c
Jaime <Jaime.Hernandez0@walmart.com>
2020-11-18 21:11:49 -0300

    base esqueleto

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/devjaime/golangrest
+
+go 1.15
+
+require (
+	github.com/gofiber/fiber v1.14.6
+	github.com/jinzhu/gorm v1.9.16
+)
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/devjaime/golangrest/book"
+	"github.com/devjaime/golangrest/database"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func helloWorld(c *fiber.Ctx) {
@@ -15,8 +20,24 @@ func setupRoutes(app *fiber.App) {
 	app.Post("/api/v1/book", book.NewBook)
 	app.Delete("/api/v1/book/:id", book.DeleteBook)
 }
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Database connection successfully opened")
+
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
+}
+
 func main() {
 	app := fiber.New()
+
+	initDatabase()
+	defer database.DBConn.Close()
 
 	setupRoutes(app)
 
